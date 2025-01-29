b1bcf6b850ab6158ba464f6483014b9804bd94be
User OnGrid <userongrid@Admins-MacBook-Pro-2.local>
2025-01-29 16:59:42 +0530

    Initial commit

---
+++ src/main/java/com/MeetingRoom/RoomM/RoomMApplication.java
@@ -0,0 +1,13 @@
+package com.MeetingRoom.RoomM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MeetingRoom/RoomM/RoomMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MeetingRoom.RoomM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
